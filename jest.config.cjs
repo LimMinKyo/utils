cbb9bdcae44441b8ac7f705b8dc82891a055efc8
ian <ian@o2pluss.com>
2023-05-16 14:56:50 +0900

    ADD: Setting Configuration
    Signed-off-by: ian <ian@o2pluss.com>

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+};
