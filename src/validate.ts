52b052a077180d0b2164dc60bf5762eeab3d0f41
LimMinKyo <story1400@naver.com>
2023-05-24 10:05:37 +0900

    ADD: isClientBrowser 유틸함수 추가
    Signed-off-by: LimMinKyo <story1400@naver.com>

--- src/validate.ts
+++ src/validate.ts
@@ -104,3 +104,13 @@ export const validateContactNumber = (value = "") => {
 
   return true;
 };
+
+/**
+ * 브라우저 환경인지 Node.js 환경인지 검사하는 유틸함수
+ */
+export const isClientBrowser = () => {
+  if (typeof window !== "undefined") {
+    return true;
+  }
+  return false;
+};
