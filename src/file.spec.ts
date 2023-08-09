911cc832a419504cd0cb76df82b7a0bbd704e6a8
LimMinKyo <story1400@naver.com>
2023-08-09 11:50:14 +0900

    ADD: downloadFileByHttp
    Signed-off-by: LimMinKyo <story1400@naver.com>

--- src/file.spec.ts
+++ src/file.spec.ts
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { downloadFile } from "./file";
+import { downloadFile, downloadFileByHttp } from "./file";
 
 describe("downloadFile", () => {
   it("should success download a file.", () => {
@@ -54,5 +54,51 @@ describe("downloadFile", () => {
 
     expect(document.body.removeChild).toHaveBeenCalledTimes(1);
     expect(document.body.removeChild).toHaveBeenCalledWith(mockLink);
+  });
+});
+
+describe("downloadFileByHttp", () => {
+  it("should success download a file.", async () => {
+    // given
+    const mockUrl = "https://test-url.com";
+    const mockLink = {
+      href: "",
+      download: "",
+      click: jest.fn(),
+      remove: jest.fn(),
+    };
+    const mockBlob = { size: 1024, type: "application/pdf" };
+    const mockData = "data";
+    const mockFileName = "test.pdf";
+    window.URL.createObjectURL = jest.fn(() => mockUrl);
+    document.createElement = jest.fn(
+      () => mockLink as unknown as HTMLAnchorElement
+    );
+    document.body.appendChild = jest.fn();
+    window.fetch = jest.fn(async () => {
+      const response: Response = {
+        blob: jest.fn(async () => mockBlob as unknown as Blob),
+      } as any;
+      return response;
+    });
+
+    // when
+    await downloadFileByHttp(mockData, mockFileName);
+
+    // then
+    expect(window.URL.createObjectURL).toHaveBeenCalledTimes(1);
+    expect(window.URL.createObjectURL).toHaveBeenCalledWith(mockBlob);
+
+    expect(document.createElement).toHaveBeenCalledTimes(1);
+    expect(document.createElement).toHaveBeenCalledWith("a");
+
+    expect(mockLink.href).toEqual(mockUrl);
+    expect(mockLink.download).toEqual(mockFileName);
+
+    expect(document.body.appendChild).toHaveBeenCalledTimes(1);
+    expect(document.body.appendChild).toHaveBeenCalledWith(mockLink);
+
+    expect(mockLink.click).toHaveBeenCalledTimes(1);
+    expect(mockLink.remove).toHaveBeenCalledTimes(1);
   });
 });
--- src/file.ts
+++ src/file.ts
@@ -12,3 +12,24 @@ export const downloadFile = (data: BlobPart, fileName: string) => {
   link.click();
   document.body.removeChild(link);
 };
+
+/**
+ * 파일 다운로드 by Http
+ * @param {string} url http 요청 주소
+ * @param {string} fileName 다운로드시 파일이름
+ */
+export const downloadFileByHttp = async (url: string, fileName: string) => {
+  return fetch(url)
+    .then((res) => {
+      return res.blob();
+    })
+    .then((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = fileName;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    });
+};
